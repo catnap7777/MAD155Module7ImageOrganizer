90c61ef44ee4318115f8a4dc9e094de5d29b08ff
kam <kmathes348@students.mchenry.edu>
2019-03-06 11:19:48 -0600

    not working

--- app/src/main/java/mad155/kmathes/mad155module7imageorganizer/MainActivity.java
+++ app/src/main/java/mad155/kmathes/mad155module7imageorganizer/MainActivity.java
@@ -1,17 +1,23 @@
 package mad155.kmathes.mad155module7imageorganizer;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,64 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.will1
             };
 
+    Integer[] cateMovies = {
+            R.drawable.cate_elizabeth1,
+            R.drawable.cate_elizabeth2,
+            R.drawable.cate_lotr_fellowship,
+            R.drawable.cate_lotr_king,
+            R.drawable.cate_hobbit,
+            R.drawable.cate_indiana_skull,
+            R.drawable.cate_monuments,
+            R.drawable.cate_oceans,
+            R.drawable.cate_ragnarok
+    };
+
+    Integer[] jenniferMovies = {
+            R.drawable.jennifer_apocalypse,
+            R.drawable.jennifer_xmen_first,
+            R.drawable.jennifer_hunger,
+            R.drawable.jennifer_hustle,
+            R.drawable.jennifer_joy,
+            R.drawable.jennifer_passengers,
+            R.drawable.jennifer_silver
+    };
+
+    Integer[] kateMovies = {
+            R.drawable.kate_chaos,
+            R.drawable.kate_divergent,
+            R.drawable.kate_dressmaker,
+            R.drawable.kate_holiday,
+            R.drawable.kate_sense,
+            R.drawable.kate_titanic
+    };
+
+    Integer[] orlandoMovies = {
+            R.drawable.orlando_lotr_fellowship,
+            R.drawable.orlando_hobbit,
+            R.drawable.orlando_kingdom,
+            R.drawable.orlando_musketeers,
+            R.drawable.orlando_pirates
+    };
+
+    Integer[] tomMovies = {
+            R.drawable.tom_big,
+            R.drawable.tom_davinci,
+            R.drawable.tom_gump,
+            R.drawable.tom_sleepless,
+            R.drawable.tom_spies,
+            R.drawable.tom_terminal,
+            R.drawable.tom_toystory
+    };
+
+    Integer[] willMovies = {
+            R.drawable.will_bright,
+            R.drawable.will_hancock,
+            R.drawable.will_happiness,
+            R.drawable.will_robot,
+            R.drawable.will_squad,
+            R.drawable.will_mib
+    };
+
     String[] celebrityName = {
             "Cate Blanchett",
             "Jennifer Lawerence",
@@ -34,6 +98,9 @@ public class MainActivity extends AppCompatActivity {
             };
 
     ImageView pic;
+
+    GridView grid2;
+    List<Integer> movieList;
 
     Button btnShowMovies;
 
@@ -56,7 +123,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         GridView grid1 = (GridView) findViewById(R.id.gridView1);
+        grid2 = (GridView) findViewById(R.id.gridView2);
+
         final ImageView pic = (ImageView) findViewById(R.id.imgLarge);
+        final ImageView pic2 = (ImageView) findViewById(R.id.imgLarge2);
         btnShowMovies = (Button) findViewById(R.id.btn1);
         final TextView celebNameTV = (TextView) findViewById(R.id.txtName);
 
@@ -71,11 +141,55 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(),"Selected Celebrity: " + (position + 1),
                         Toast.LENGTH_SHORT).show();
 
+
                 pic.setImageResource(celebrityPic[position]);
                 celebNameTV.setText(celebrityName[position]);
+
+                switch(position) {
+                    case 0:
+                        movieList = new ArrayList<>(Arrays.asList(cateMovies));
+                        pic2.setImageResource(movieList.get(7));
+                        System.out.println("IN THE CATE ARRAY");
+                        for(Integer kam: movieList){
+                            System.out.println("movie in list: " + kam);
+                        }
+                        break;
+                    case 1:
+                        movieList = new ArrayList<>(Arrays.asList(jenniferMovies));
+                        pic2.setImageResource(movieList.get(3));
+                        System.out.println("IN THE JENNIFER ARRAY");
+                        for(Integer kam: movieList){
+                            System.out.println("movie in list: " + kam);
+                        }
+                        break;
+                    case 2:
+                        movieList = new ArrayList<>(Arrays.asList(kateMovies));
+                        break;
+                    case 3:
+                        movieList = new ArrayList<>(Arrays.asList(orlandoMovies));
+                        break;
+                    case 4:
+                        movieList = new ArrayList<>(Arrays.asList(tomMovies));
+                        break;
+                    case 5:
+                        movieList = new ArrayList<>(Arrays.asList(willMovies));
+                        break;
+                    default:
+                        System.out.println("OOPS!!! BIG PROBLEM");
+                        break;
+                }
+
+                //grid2.setAdapter(new ImageAdapter2(this));
             }
         });
 
+        if(movieList == null || movieList.isEmpty()){
+            System.out.println("MOVIELIST LIST IS EMPTY OR NULL");
+        } else {
+            grid2.setAdapter(new ImageAdapter2(this));
+
+        }
+        //grid2.setAdapter(new ImageAdapter2(this));
 
     }
 
@@ -121,5 +235,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class ImageAdapter2 extends BaseAdapter {
 
+        private Context context;
+
+        public ImageAdapter2(Context c) {
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            //return movieList.length;
+            return movieList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ImageView pic2 = new ImageView(context);
+            for (Integer movie: movieList) {
+                pic2.setImageResource(movie);
+            }
+            //pic2.setImageResource(movieList[position]);
+            pic2.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic2.setLayoutParams(new GridView.LayoutParams(330,375));
+            return pic2;
+        }
+    }
 }
